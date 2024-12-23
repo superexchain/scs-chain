bd4f747e2e8da56d89ce0dfd69c3aafe2153c5bd
weimeme <weimeme067@gmail.com>
2024-12-23 15:04:03 +0800

    update scs-chain-spec

--- scripts/generate_chain_spec.sh
+++ scripts/generate_chain_spec.sh
@@ -2,3 +2,5 @@
 
 # after generate node key
 ./target/release/scs build-spec --raw  --base-path db --chain staging --disable-default-bootnode > chain-spec.json
+
+./target/release/scs build-spec --raw  --base-path db --chain tscs-local --disable-default-bootnode > tscs-chain-spec.json
